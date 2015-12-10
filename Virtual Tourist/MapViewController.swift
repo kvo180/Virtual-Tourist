5f68e03121bd3ccce56cb84b800ea4855110ba9c
kvo180 <khoavo180@gmail.com>
2015-12-09 22:24:56 -0800

    Add ability to drag pin when first creating (before dropping)
    - Prevent transition to PhotoAlbumVC after dragging annotation view. Transition only occurs if annotation view is selected by tapping (instead of dragging).

--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -13,20 +13,21 @@ class MapViewController: UIViewController, MKMapViewDelegate {
 
     // MARK:  Properties
     
-    @IBOutlet weak var travelLocationsMapView: MKMapView!
-    
+    @IBOutlet weak var mapView: MKMapView!
+    var droppedPin: MKPointAnnotation!
+    var dragged = false
     
     // MARK: - UI Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        travelLocationsMapView.delegate = self
+        mapView.delegate = self
         
         // Add and configure gesture recognizers
         let longPressGesture = UILongPressGestureRecognizer(target: self, action: "addPin:")
         longPressGesture.minimumPressDuration = 0.5
-        travelLocationsMapView.addGestureRecognizer(longPressGesture)
+        mapView.addGestureRecognizer(longPressGesture)
     
     }
     
@@ -35,31 +36,57 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         
     }
     
-    
     // MARK: - Actions
     
     func addPin(gestureRecognizer: UIGestureRecognizer) {
         
-        if gestureRecognizer.state == UIGestureRecognizerState.Began {
-            
-            // Get point from gesture recognizer and convert to map coordinates
-            let point = gestureRecognizer.locationInView(travelLocationsMapView)
-            let coordinates = travelLocationsMapView.convertPoint(point, toCoordinateFromView: travelLocationsMapView)
+        // Get point from gesture recognizer and convert to map coordinates
+        let point = gestureRecognizer.locationInView(mapView)
+        let coordinates = mapView.convertPoint(point, toCoordinateFromView: mapView)
+        
+        switch gestureRecognizer.state {
+        case .Began:
+            print("gesture began")
             
             // Create annotation from coordinates and add to map view
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = coordinates
-            annotation.title = "pin selected"
-            travelLocationsMapView.addAnnotation(annotation)
+            droppedPin = MKPointAnnotation()
+            droppedPin.coordinate = coordinates
+            
+            // Set title property to make annotation draggable
+            droppedPin.title = "pin selected"
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                self.mapView.addAnnotation(self.droppedPin)
+            }
+            
+        case .Changed:
+            print("gesture changed")
+            
+            if droppedPin != nil {
+                // Update annotation coordinates
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.droppedPin.coordinate = coordinates
+                    print(self.droppedPin.coordinate)
+                }
+            }
+            
+        case .Ended:
+            print("gesture ended")
+            
+            // Save pin and fetch images
+            
+        default:
+            return
         }
     }
+    
+    
+    // MARK: - Helper Methods
+    
 
 
     // MARK: MKMapViewViewDelegate Methods
     func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
-        if annotation is MKUserLocation {
-            return nil
-        }
         
         let reuseId = "pin"
         
@@ -72,6 +99,7 @@ class MapViewController: UIViewController, MKMapViewDelegate {
             pinView!.canShowCallout = false
             pinView!.pinTintColor = UIColor.redColor()
             
+            // Set annotation as selected so that it is draggable after being created
             pinView!.setSelected(true, animated: true)
         }
         else {
@@ -83,25 +111,43 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     
     func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, didChangeDragState newState: MKAnnotationViewDragState, fromOldState oldState: MKAnnotationViewDragState) {
         
-        if newState == MKAnnotationViewDragState.Ending {
+        switch newState {
+        case .Starting:
+            print("drag starting")
+            
+            // Delete old photos from pin
+            
+        case .Canceling, .Ending:
             let draggedAnnotation = view.annotation
             print("annotation dropped at: \(draggedAnnotation!.coordinate.latitude), \(draggedAnnotation!.coordinate.longitude)")
+            
+            // Since annotation view will be automatically selected after dragging occurs, set to true so that push to PhotoAlbumViewController doesn't occur
+            dragged = true
+
+            // Get new photos for pin
+            
+        default:
+            return
         }
     }
     
     func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
         print("annotation view selected")
         
-        // Push PhotoAlbumViewController to top of stack
-        let controller = storyboard!.instantiateViewControllerWithIdentifier("PhotoAlbumViewController") as! PhotoAlbumViewController
-        navigationController!.pushViewController(controller, animated: true)
-        
         // Deselect annotation from map view so that it can be tapped again without having to first be deselected by the user
-        let selectedAnnotations = mapView.selectedAnnotations
-        travelLocationsMapView.deselectAnnotation(selectedAnnotations[0], animated: false)
+        mapView.deselectAnnotation(view.annotation, animated: false)
         
-        // Set annotation view to selected so that it is draggable
+        // Set annotation view as selected so that it is draggable
         view.setSelected(true, animated: true)
+        
+        // If annotation view is selected by tapping (instead of by dragging), push PhotoAlbumViewController to top of nav stack
+        if dragged == false {
+            let controller = storyboard!.instantiateViewControllerWithIdentifier("PhotoAlbumViewController") as! PhotoAlbumViewController
+            navigationController!.pushViewController(controller, animated: true)
+        } else {
+            // Set to false to enable push to PhotoAlbumViewController by tapping on annotation view
+            dragged = false
+        }
     }
     
     func mapView(mapView: MKMapView, didDeselectAnnotationView view: MKAnnotationView) {
